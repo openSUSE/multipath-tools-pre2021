84c86abbdfcdf991a444ba219f457e1c1a53b2a3
Hannes Reinecke <hare@suse.de>
2008-02-04 09:06:12 +0100

    Whitespace cleanup
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -48,7 +48,7 @@ dm_write_log (int level, const char *file, int line, const char *f, ...)
 	if (thres <= 3 || level > thres)
 		return;
 
-        va_start(ap, f);
+	va_start(ap, f);
 #if DAEMON
 	if (!logsink) {
 		time_t t = time(NULL);
@@ -71,7 +71,7 @@ dm_write_log (int level, const char *file, int line, const char *f, ...)
 	vfprintf(stdout, f, ap);
 	fprintf(stdout, "\n");
 #endif
-        va_end(ap);
+	va_end(ap);
 
 	return;
 }
@@ -268,11 +268,11 @@ dm_get_uuid(char *name, char *uuid)
 	if (!dmt)
 		return 1;
 
-        if (!dm_task_set_name (dmt, name))
-                goto uuidout;
+	if (!dm_task_set_name (dmt, name))
+		goto uuidout;
 
 	if (!dm_task_run(dmt))
-                goto uuidout;
+		goto uuidout;
 
 	uuidtmp = dm_task_get_uuid(dmt);
 	if (uuidtmp) {
@@ -394,7 +394,7 @@ out:
 	dm_task_destroy(dmt);
 	return r;
 }
-	
+
 int
 dm_get_opencount (char * mapname)
 {
@@ -419,7 +419,7 @@ out:
 	dm_task_destroy(dmt);
 	return r;
 }
-	
+
 int
 dm_get_minor (char * mapname)
 {
@@ -444,7 +444,7 @@ out:
 	dm_task_destroy(dmt);
 	return r;
 }
-	
+
 extern int
 dm_flush_map (char * mapname, char * type)
 {
@@ -462,7 +462,7 @@ dm_flush_map (char * mapname, char * type)
 	if (dm_get_opencount(mapname)) {
 		condlog(2, "%s: map in use", mapname);
 		return 1;
-	}	
+	}
 
 	r = dm_simplecmd(DM_DEVICE_REMOVE, mapname);
 
@@ -674,8 +674,8 @@ dm_get_maps (vector mp, char * type)
 		vector_set_slot(mp, mpp);
 		mpp = NULL;
 next:
-                next = names->next;
-                names = (void *) names + next;
+		next = names->next;
+		names = (void *) names + next;
 	} while (next);
 
 	r = 0;
@@ -855,7 +855,7 @@ dm_remove_partmaps (char * mapname)
 		     */
 		    strstr(params, dev_t)
 		   ) {
-		    		/*
+				/*
 				 * then it's a kpartx generated partition.
 				 * remove it.
 				 */
@@ -885,7 +885,7 @@ dm_get_info (char * mapname, struct dm_info ** dmi)
 {
 	int r = 1;
 	struct dm_task *dmt = NULL;
-	
+
 	if (!mapname)
 		return 1;
 
@@ -973,7 +973,7 @@ dm_rename_partmaps (char * old, char * new)
 		     */
 		    strstr(buff, dev_t)
 		   ) {
-		    		/*
+				/*
 				 * then it's a kpartx generated partition.
 				 * Rename it.
 				 */
@@ -1011,7 +1011,7 @@ dm_rename (char * old, char * new)
 
 	if (!dm_task_set_newname(dmt, new))
 		goto out;
-	
+
 	dm_task_no_open_count(dmt);
 
 	if (!dm_task_run(dmt))
