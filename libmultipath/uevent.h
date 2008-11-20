e8f4746fa0aec8422022f52d917d6f82915128ce
Hannes Reinecke <hare@suse.de>
2008-11-20 12:14:42 +0100

    Rework sysfs device handling in multipathd
    Relying on sysfs devices has the disadvantage that the device
    might already been gone by the time we look at it. And we don't
    actually need it for eg device-mapper events as we can get all
    required information via the device-mapper ioctl.
    So only access sysfs if we absolutely have to and try to get
    the information from other places if possible.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/uevent.h
+++ libmultipath/uevent.h
@@ -1,3 +1,6 @@
+#ifndef _UEVENT_H
+#define _UEVENT_H
+
 /* environment buffer, the kernel's size in lib/kobject_uevent.c should fit in */
 #define HOTPLUG_BUFFER_SIZE		1024
 #define HOTPLUG_NUM_ENVP		32
@@ -12,8 +15,13 @@ struct uevent {
 	char buffer[HOTPLUG_BUFFER_SIZE + OBJECT_SIZE];
 	char *devpath;
 	char *action;
+	char *kernel;
 	char *envp[HOTPLUG_NUM_ENVP];
 };
 
 int uevent_listen(int (*store_uev)(struct uevent *, void * trigger_data),
 		  void * trigger_data);
+int uevent_get_major(struct uevent *uev);
+int uevent_get_minor(struct uevent *uev);
+
+#endif /* _UEVENT_H */
