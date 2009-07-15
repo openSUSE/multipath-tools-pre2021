f0acb2cb53f0a65b57bec1bd5f7ddc46bcec7cc7
Hannes Reinecke <hare@suse.de>
2009-07-15 11:38:11 +0200

    Update features when queue_if_no_path is set
    When queue_if_no_path is set directly, the 'features' entry
    is not updated until after a new table is read from devicemapper.
    So for multipath it's not updated at all and give an incorrect
    output.
    
    References: bnc#511607
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/dmparser.c
+++ libmultipath/dmparser.c
@@ -44,183 +44,6 @@ merge_words (char ** dst, char * word, int space)
 	return 0;
 }
 
-static int
-add_feature (char **f, char *n)
-{
-	int c = 0, d, l;
-	char *e, *p, *t;
-
-	if (!f)
-		return 1;
-
-	/* Nothing to do */
-	if (!n || *n == '0')
-		return 0;
-
-	/* Check if feature is already present */
-	if (strstr(*f, n))
-		return 0;
-
-	/* Get feature count */
-	c = strtoul(*f, &e, 10);
-	if (*f == e)
-		/* parse error */
-		return 1;
-
-	/* Check if we need to increase feature count space */
-	l = strlen(*f) + strlen(n) + 1;
-
-	/* Count new features */
-	if ((c % 10) == 9)
-		l++;
-	c++;
-	p = n;
-	while (*p != '\0') {
-		if (*p == ' ' && p[1] != '\0' && p[1] != ' ') {
-			if ((c % 10) == 9)
-				l++;
-			c++;
-		}
-		p++;
-	}
-
-	t = MALLOC(l + 1);
-	if (!t)
-		return 1;
-
-	memset(t, 0, l + 1);
-
-	/* Update feature count */
-	d = c;
-	l = 1;
-	while (d > 9) {
-		d /= 10;
-		l++;
-	}
-	p = t;
-	snprintf(p, l + 2, "%0d ", c);
-
-	/* Copy the feature string */
-	p = strchr(*f, ' ');
-	if (p) {
-		while (*p == ' ')
-			p++;
-		strcat(t, p);
-		strcat(t, " ");
-	} else {
-		p = t + strlen(t);
-	}
-	strcat(t, n);
-
-	FREE(*f);
-	*f = t;
-
-	return 0;
-}
-
-static int remove_feature(char **f, char *o)
-{
-	int c = 0, d, l;
-	char *e, *p, *n;
-
-	if (!f || !*f)
-		return 1;
-
-	/* Nothing to do */
-	if (!o || *o == '\0')
-		return 0;
-
-	/* Check if not present */
-	if (!strstr(*f, o))
-		return 0;
-
-	/* Get feature count */
-	c = strtoul(*f, &e, 10);
-	if (*f == e)
-		/* parse error */
-		return 1;
-
-	/* Normalize features */
-	while (*o == ' ') {
-		o++;
-	}
-	/* Just spaces, return */
-	if (*o == '\0')
-		return 0;
-	e = o + strlen(o);
-	while (*e == ' ')
-		e--;
-	d = (int)(e - o);
-
-	/* Update feature count */
-	c--;
-	p = o;
-	while (p[0] != '\0') {
-		if (p[0] == ' ' && p[1] != ' ' && p[1] != '\0')
-			c--;
-		p++;
-	}
-
-	/* Quick exit if all features have been removed */
-	if (c == 0) {
-		n = MALLOC(2);
-		if (!n)
-			return 1;
-		strcpy(n, "0");
-		goto out;
-	}
-
-	/* Search feature to be removed */
-	e = strstr(*f, o);
-	if (!e)
-		/* Not found, return */
-		return 0;
-
-	/* Update feature count space */
-	l = strlen(*f) - d;
-	n =  MALLOC(l + 1);
-	if (!n)
-		return 1;
-
-	/* Copy the feature count */
-	sprintf(n, "%0d", c);
-	/*
-	 * Copy existing features up to the feature
-	 * about to be removed
-	 */
-	p = strchr(*f, ' ');
-	while (*p == ' ')
-		p++;
-	p--;
-	if (e != p) {
-		do {
-			e--;
-			d++;
-		} while (*e == ' ');
-		e++; d--;
-		strncat(n, p, (size_t)(e - p));
-		p += (size_t)(e - p);
-	}
-	/* Skip feature to be removed */
-	p += d;
-
-	/* Copy remaining features */
-	if (strlen(p)) {
-		while (*p == ' ')
-			p++;
-		if (strlen(p)) {
-			p--;
-			strcat(n, p);
-		}
-	}
-
-out:
-	FREE(*f);
-	*f = n;
-
-	return 0;
-}
-
 /*
  * Transforms the path group vector into a proper device map string
  */
@@ -319,6 +142,7 @@ disassemble_map (vector pathvec, char * params, struct multipath * mpp)
 	int num_paths = 0;
 	int num_paths_args = 0;
 	int def_minio = 0;
+	int no_path_retry = NO_PATH_RETRY_UNDEF;
 	struct path * pp;
 	struct pathgroup * pgp;
 
@@ -335,6 +159,7 @@ disassemble_map (vector pathvec, char * params, struct multipath * mpp)
 		return 1;
 
 	num_features = atoi(mpp->features);
+	no_path_retry = mpp->no_path_retry;
 
 	for (i = 0; i < num_features; i++) {
 		p += get_word(p, &word);
@@ -346,12 +171,14 @@ disassemble_map (vector pathvec, char * params, struct multipath * mpp)
 			FREE(word);
 			return 1;
 		}
-		if ((mpp->no_path_retry == NO_PATH_RETRY_UNDEF) ||
-			(mpp->no_path_retry == NO_PATH_RETRY_FAIL) ||
-			(mpp->no_path_retry == NO_PATH_RETRY_QUEUE))
-			setup_feature(mpp, word);
+		setup_feature(mpp, word);
+
 		FREE(word);
 	}
+	/* Update no_path_retry if not set from features */
+	if (no_path_retry == NO_PATH_RETRY_QUEUE &&
+	    no_path_retry == mpp->no_path_retry)
+		mpp->no_path_retry = NO_PATH_RETRY_UNDEF;
 
 	/*
 	 * hwhandler
