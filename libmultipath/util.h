a5e418bf6263132411479cf1c48d62791eedba2c
Hannes Reinecke <hare@suse.de>
2010-05-28 10:59:25 +0200

    libmultipath: move devt2devname to util.c
    It really belongs to there, not to the discovery.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/util.h
+++ libmultipath/util.h
@@ -8,6 +8,7 @@ int get_word (char * sentence, char ** word);
 size_t strlcpy(char *dst, const char *src, size_t size);
 size_t strlcat(char *dst, const char *src, size_t size);
 void remove_trailing_chars(char *path, char c);
+int devt2devname (char *, char *);
 
 #define safe_sprintf(var, format, args...)	\
 	snprintf(var, sizeof(var), format, ##args) >= sizeof(var)
