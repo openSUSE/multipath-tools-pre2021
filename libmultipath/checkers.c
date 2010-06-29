0fa3ffcfd66311277a55ce000631bf0a0c3772ff
Hannes Reinecke <hare@suse.de>
2010-06-29 10:52:09 +0200

    Reset checker message before calling checker_check()
    We shouldn't rely on the individual checkers to reset the checker
    message. Rather reset it explicitely prior to calling the checkers.
    
    References: bnc#613330
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/checkers.c
+++ libmultipath/checkers.c
@@ -192,8 +192,11 @@ int checker_check (struct checker * c)
 {
 	int r;
 
-	if (c->disable)
+	c->message[0] = '\0';
+	if (c->disable) {
+		MSG(c, "checker disabled");
 		return PATH_UNCHECKED;
+	}
 	if (c->fd <= 0) {
 		MSG(c, "no usable fd");
 		return PATH_WILD;
