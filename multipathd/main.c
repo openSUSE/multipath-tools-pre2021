d87db7714fb620c8002c502263211a6d60634ec3
Hannes Reinecke <hare@suse.de>
2011-09-23 15:42:38 +0200

    Recheck dm-multipath path priorities after failed paths recover
    During priority recheck the paths might be zero, so check
    for it appropriately.
    
    References: bnc#694588
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- multipathd/main.c
+++ multipathd/main.c
@@ -853,6 +853,7 @@ retry_count_tick(vector mpvec)
 int update_prio(struct path *pp, int refresh_all)
 {
 	int oldpriority;
+	struct path *pp1;
 	struct pathgroup * pgp;
 	int i, j, changed = 0;
 
@@ -860,9 +861,11 @@ int update_prio(struct path *pp, int refresh_all)
 		if (!pp->mpp)
 			return 0;
 		vector_foreach_slot (pp->mpp->pg, pgp, i) {
-			vector_foreach_slot (pgp->paths, pp, j) {
+			vector_foreach_slot (pgp->paths, pp1, j) {
 				oldpriority = pp->priority;
-				pathinfo(pp, conf->hwtable, DI_PRIO);
+				if (pp1->state == PATH_UP||
+				    pp1->state == PATH_GHOST)
+					pathinfo(pp, conf->hwtable, DI_PRIO);
 				if (pp->priority != oldpriority)
 					changed = 1;
 			}
@@ -897,7 +900,8 @@ int update_path_groups(struct multipath *mpp, struct vectors *vecs, int refresh)
 		return 1;
 	}
 	dm_lib_release();
-	setup_multipath(vecs, mpp);
+	if (setup_multipath(vecs, mpp) != 0)
+		return 1;
 	sync_map_state(mpp);
 
 	return 0;
