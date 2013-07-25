26d0718a33b33ae303565ba3d868671f2296fede
Hannes Reinecke <hare@suse.de>
2013-07-25 13:00:42 +0200

    multipathd: Add option '-s' to suppress timestamps
    systemd prefixes any messages to stdout with a timestamp, so it's
    quite pointless to do it ourself.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- multipathd/main.c
+++ multipathd/main.c
@@ -1598,7 +1598,7 @@ child (void * param)
 	setup_thread_attr(&uevent_attr, 128 * 1024, 1);
 	setup_thread_attr(&waiter_attr, 32 * 1024, 1);
 
-	if (logsink) {
+	if (logsink == 1) {
 		setup_thread_attr(&log_attr, 64 * 1024, 0);
 		log_thread_start(&log_attr);
 		pthread_attr_destroy(&log_attr);
@@ -1754,7 +1754,7 @@ child (void * param)
 
 	condlog(2, "--------shut down-------");
 
-	if (logsink)
+	if (logsink == 1)
 		log_thread_stop();
 
 	/*
@@ -1854,7 +1854,7 @@ main (int argc, char *argv[])
 	if (!conf)
 		exit(1);
 
-	while ((arg = getopt(argc, argv, ":dv:k::")) != EOF ) {
+	while ((arg = getopt(argc, argv, ":dsv:k::")) != EOF ) {
 	switch(arg) {
 		case 'd':
 			logsink = 0;
@@ -1866,6 +1866,9 @@ main (int argc, char *argv[])
 				exit(1);
 
 			conf->verbosity = atoi(optarg);
+			break;
+		case 's':
+			logsink = -1;
 			break;
 		case 'k':
 			uxclnt(optarg);
@@ -1891,7 +1894,7 @@ main (int argc, char *argv[])
 		exit(0);
 	}
 
-	if (!logsink)
+	if (logsink < 1)
 		err = 0;
 	else
 		err = daemonize();
