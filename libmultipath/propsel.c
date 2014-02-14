2d2c4d9597213b44c403e4cc814f5ddd5da80a1a
Hannes Reinecke <hare@suse.de>
2014-02-14 10:11:24 +0100

    libmultipath: Prefer deprecated 'getuid' callout
    We need first to check if the deprecated 'getuid' callout
    should be used, otherwise it might never be called.
    
    References: bnc#861534
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/propsel.c
+++ libmultipath/propsel.c
@@ -365,28 +365,28 @@ out:
 extern int
 select_getuid (struct path * pp)
 {
-	if (pp->hwe && pp->hwe->uid_attribute) {
-		pp->uid_attribute = pp->hwe->uid_attribute;
-		condlog(3, "%s: uid_attribute = %s (controller setting)",
-			pp->dev, pp->uid_attribute);
-		return 0;
-	}
 	if (pp->hwe && pp->hwe->getuid) {
 		pp->getuid = pp->hwe->getuid;
 		condlog(3, "%s: getuid = %s (deprecated) (controller setting)",
 			pp->dev, pp->getuid);
 		return 0;
 	}
-	if (conf->uid_attribute) {
-		pp->uid_attribute = conf->uid_attribute;
-		condlog(3, "%s: uid_attribute = %s (config file default)",
-			pp->dev, pp->uid_attribute);
-		return 0;
-	}
 	if (conf->getuid) {
 		pp->getuid = conf->getuid;
 		condlog(3, "%s: getuid = %s (deprecated) (config file default)",
 			pp->dev, pp->getuid);
+		return 0;
+	}
+	if (pp->hwe && pp->hwe->uid_attribute) {
+		pp->uid_attribute = pp->hwe->uid_attribute;
+		condlog(3, "%s: uid_attribute = %s (controller setting)",
+			pp->dev, pp->uid_attribute);
+		return 0;
+	}
+	if (conf->uid_attribute) {
+		pp->uid_attribute = conf->uid_attribute;
+		condlog(3, "%s: uid_attribute = %s (config file default)",
+			pp->dev, pp->uid_attribute);
 		return 0;
 	}
 	pp->uid_attribute = STRDUP(DEFAULT_UID_ATTRIBUTE);
