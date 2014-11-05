a91a545f1e982c24ca96dda151368d80931bf9d7
Hannes Reinecke <hare@suse.de>
2014-11-05 14:01:11 +0100

    devmapper: do not flush I/O for DM_DEVICE_CREATE
    DM_DEVICE_CREATE loads a new table, so there cannot be any
    I/O pending. Hence we should be setting the 'no flush'
    and 'skip lockfs' flag to avoid delays during creation.
    
    References: bnc#901809
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -292,13 +292,19 @@ dm_addmap (int task, const char *target, struct multipath *mpp, char * params,
 
 	dm_task_no_open_count(dmt);
 
+	if (task == DM_DEVICE_CREATE) {
 #ifdef LIBDM_API_COOKIE
-	if (task == DM_DEVICE_CREATE &&
-	    !dm_task_set_cookie(dmt, &cookie, (conf->daemon)? DM_UDEV_DISABLE_LIBRARY_FALLBACK : 0)) {
-		dm_udev_complete(cookie);
-		goto freeout;
-	}
+		if (!dm_task_set_cookie(dmt, &cookie,
+					(conf->daemon) ? DM_UDEV_DISABLE_LIBRARY_FALLBACK : 0)) {
+			dm_udev_complete(cookie);
+			goto freeout;
+		}
 #endif
+		dm_task_skip_lockfs(dmt);
+#ifdef LIBDM_API_FLUSH
+		dm_task_no_flush(dmt);
+#endif
+	}
 	r = dm_task_run (dmt);
 
 #ifdef LIBDM_API_COOKIE
