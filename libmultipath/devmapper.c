bfa4859fceff4d41cb0b1a990858125b9af42ddb
Benjamin Marzinski <bmarzins@redhat.com>
2015-03-07 21:31:35 -0600

    libmultipath: refactor partmaps code
    There was a lot of code duplication between dm_remove_partmaps and
    dm_rename_partmaps. This patch factors that out into
    do_foreach_partmaps.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -1034,8 +1034,9 @@ bad:
 	return NULL;
 }
 
-int
-dm_remove_partmaps (const char * mapname, int need_sync)
+static int
+do_foreach_partmaps (const char * mapname, int (*partmap_func)(char *, void *),
+		     void *data)
 {
 	struct dm_task *dmt;
 	struct dm_names *names;
@@ -1087,25 +1088,8 @@ dm_remove_partmaps (const char * mapname, int need_sync)
 		     */
 		    strstr(params, dev_t)
 		   ) {
-			/*
-			 * then it's a kpartx generated partition.
-			 * remove it.
-			 */
-			/*
-			 * if the opencount is 0 maybe some other
-			 * partitions depend on it.
-			 */
-			if (dm_get_opencount(names->name)) {
-				dm_remove_partmaps(names->name, need_sync);
-				if (dm_get_opencount(names->name)) {
-					condlog(2, "%s: map in use",
-						names->name);
-					goto out;
-				}
-			}
-			condlog(4, "partition map %s removed",
-				names->name);
-			dm_simplecmd_flush(DM_DEVICE_REMOVE, names->name, 0);
+			if (partmap_func(names->name, data) != 0)
+				goto out;
 		}
 
 		next = names->next;
@@ -1116,6 +1100,34 @@ dm_remove_partmaps (const char * mapname, int need_sync)
 out:
 	dm_task_destroy (dmt);
 	return r;
+}
+
+struct remove_data {
+	int need_sync;
+};
+
+static int
+remove_partmap(char *name, void *data)
+{
+	struct remove_data *rd = (struct remove_data *)data;
+
+	if (dm_get_opencount(name)) {
+		dm_remove_partmaps(name, rd->need_sync);
+		if (dm_get_opencount(name)) {
+			condlog(2, "%s: map in use", name);
+			return 1;
+		}
+	}
+	condlog(4, "partition map %s removed", name);
+	dm_simplecmd_flush(DM_DEVICE_REMOVE, name, 0);
+	return 0;
+}
+
+int
+dm_remove_partmaps (const char * mapname, int need_sync)
+{
+	struct remove_data rd = { need_sync };
+	return do_foreach_partmaps(mapname, remove_partmap, &rd);
 }
 
 static struct dm_info *
@@ -1167,78 +1179,34 @@ out:
 	return r;
 }
 
+struct rename_data {
+	char *old;
+	char *new;
+};
+
+static int
+rename_partmap (char *name, void *data)
+{
+	char buff[PARAMS_SIZE];
+	struct rename_data *rd = (struct rename_data *)data;
+
+	if (strncmp(name, rd->old, strlen(rd->old)) != 0)
+		return 0;
+	snprintf(buff, PARAMS_SIZE, "%s%s", rd->new, name + strlen(rd->old));
+	dm_rename(name, buff);
+	condlog(4, "partition map %s renamed", name);
+	return 0;
+}
+
 int
 dm_rename_partmaps (char * old, char * new)
 {
-	struct dm_task *dmt;
-	struct dm_names *names;
-	unsigned next = 0;
-	char buff[PARAMS_SIZE];
-	unsigned long long size;
-	char dev_t[32];
-	int r = 1;
+	struct rename_data rd;
 
-	if (!(dmt = dm_task_create(DM_DEVICE_LIST)))
-		return 1;
+	rd.old = old;
+	rd.new = new;
 
-	dm_task_no_open_count(dmt);
-
-	if (!dm_task_run(dmt))
-		goto out;
-
-	if (!(names = dm_task_get_names(dmt)))
-		goto out;
-
-	if (!names->dev) {
-		r = 0; /* this is perfectly valid */
-		goto out;
-	}
-
-	if (dm_dev_t(old, &dev_t[0], 32))
-		goto out;
-
-	do {
-		if (
-		    /*
-		     * if devmap target is "linear"
-		     */
-		    (dm_type(names->name, TGT_PART) > 0) &&
-
-		    /*
-		     * and the multipath mapname and the part mapname start
-		     * the same
-		     */
-		    !strncmp(names->name, old, strlen(old)) &&
-
-		    /*
-		     * and we can fetch the map table from the kernel
-		     */
-		    !dm_get_map(names->name, &size, &buff[0]) &&
-
-		    /*
-		     * and the table maps over the multipath map
-		     */
-		    strstr(buff, dev_t)
-		   ) {
-				/*
-				 * then it's a kpartx generated partition.
-				 * Rename it.
-				 */
-				snprintf(buff, PARAMS_SIZE, "%s%s",
-					 new, names->name + strlen(old));
-				dm_rename(names->name, buff);
-				condlog(4, "partition map %s renamed",
-					names->name);
-		   }
-
-		next = names->next;
-		names = (void *) names + next;
-	} while (next);
-
-	r = 0;
-out:
-	dm_task_destroy (dmt);
-	return r;
+	return do_foreach_partmaps(old, rename_partmap, &rd);
 }
 
 int
