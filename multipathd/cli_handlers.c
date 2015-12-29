871288ea9e7d96f607bd353510c51208cd9b1072
tangjunhui <110950397@qq.com>
2015-12-29 19:06:43 +0800

    Update cli_handlers.c

--- multipathd/cli_handlers.c
+++ multipathd/cli_handlers.c
@@ -885,24 +885,24 @@ cli_resume(void * v, char ** reply, int * len, void * data)
 int
 cli_reinstate(void * v, char ** reply, int * len, void * data)
 {
-        struct vectors * vecs = (struct vectors *)data;
-        char * param = get_keyparam(v, PATH);
-        struct path * pp;
+	struct vectors * vecs = (struct vectors *)data;
+	char * param = get_keyparam(v, PATH);
+	struct path * pp;
 
-        param = convert_dev(param, 1);
-        pp = find_path_by_dev(vecs->pathvec, param);
+	param = convert_dev(param, 1);
+	pp = find_path_by_dev(vecs->pathvec, param);
 
-        if (!pp)
-                 pp = find_path_by_devt(vecs->pathvec, param);
+	if (!pp)
+		 pp = find_path_by_devt(vecs->pathvec, param);
 
-        if (!pp || !pp->mpp || !pp->mpp->alias)
-                return 1;
+	if (!pp || !pp->mpp || !pp->mpp->alias)
+		return 1;
 
-        condlog(2, "%s: reinstate path %s (operator)",
-                pp->mpp->alias, pp->dev_t);
+	condlog(2, "%s: reinstate path %s (operator)",
+		pp->mpp->alias, pp->dev_t);
 
-        checker_enable(&pp->checker);
-        return dm_reinstate_path(pp->mpp->alias, pp->dev_t);
+	checker_enable(&pp->checker);
+	return dm_reinstate_path(pp->mpp->alias, pp->dev_t);
 }
 
 int
