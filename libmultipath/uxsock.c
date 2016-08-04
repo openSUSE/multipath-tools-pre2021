31ef8d3446442076a44296807b4e9873a515f34c
Hannes Reinecke <hare@suse.de>
2016-08-04 13:47:54 +0200

    libmultipath: remove unused function 'read_all'
    Function got moved to libmpathcmd, but was forgotten
    to be deleted here.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>

--- libmultipath/uxsock.c
+++ libmultipath/uxsock.c
@@ -97,43 +97,6 @@ size_t write_all(int fd, const void *buf, size_t len)
 }
 
 /*
- * keep reading until its all read
- */
-ssize_t read_all(int fd, void *buf, size_t len, unsigned int timeout)
-{
-	size_t total = 0;
-	ssize_t n;
-	int ret;
-	struct pollfd pfd;
-
-	while (len) {
-		pfd.fd = fd;
-		pfd.events = POLLIN;
-		ret = poll(&pfd, 1, timeout);
-		if (!ret) {
-			return -ETIMEDOUT;
-		} else if (ret < 0) {
-			if (errno == EINTR)
-				continue;
-			return -errno;
-		} else if (!pfd.revents & POLLIN)
-			continue;
-		n = read(fd, buf, len);
-		if (n < 0) {
-			if ((errno == EINTR) || (errno == EAGAIN))
-				continue;
-			return -errno;
-		}
-		if (!n)
-			return total;
-		buf = n + (char *)buf;
-		len -= n;
-		total += n;
-	}
-	return total;
-}
-
-/*
  * send a packet in length prefix format
  */
 int send_packet(int fd, const char *buf)
