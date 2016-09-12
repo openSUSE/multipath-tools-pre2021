opensvc/multipath-tools

multipathd: read-only bindings
Multipath already uses -B to run in a mode where it won't write the the
bindings file (which can be useful in the initramfs). Its simple to make
multipathd able to work this way as well.

Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

multipathd/main.c
@@ -1971,7 +1971,7 @@ main (int argc, char *argv[])
 	if (!conf)
 		exit(1);
 
-	while ((arg = getopt(argc, argv, ":dsv:k::")) != EOF ) {
+	while ((arg = getopt(argc, argv, ":dsv:k::B")) != EOF ) {
 	switch(arg) {
 		case 'd':
 			logsink = 0;
@@ -1990,6 +1990,9 @@ main (int argc, char *argv[])
 		case 'k':
 			uxclnt(optarg);
 			exit(0);
+		case 'B':
+			conf->bindings_read_only = 1;
+			break;
 		default:
 			;
 		}

openSUSE/multipath-tools-pre2021

multipathd/main.c
@@ -1903,7 +1903,7 @@ main (int argc, char *argv[])
 	if (!conf)
 		exit(1);
 
-	while ((arg = getopt(argc, argv, ":dv:k::")) != EOF ) {
+	while ((arg = getopt(argc, argv, ":dv:k::B")) != EOF ) {
 	switch(arg) {
 		case 'd':
 			logsink = 0;
@@ -1919,6 +1919,9 @@ main (int argc, char *argv[])
 		case 'k':
 			uxclnt(optarg);
 			exit(0);
+		case 'B':
+			conf->bindings_read_only = 1;
+			break;
 		default:
 			;
 		}