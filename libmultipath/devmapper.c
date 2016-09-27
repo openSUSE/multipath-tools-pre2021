aea8f3527bd25bb6197550d854faaa536432c01f
Martin Wilck <mwilck@suse.de>
2016-09-27 17:14:37 +0200

    _dm_flush_map: add "need_suspend" parameter
    This parameter controls whether _dm_flush_map will set
    queue_if_no_path = 0 and suspend the map before removing
    it, trying to avoid any outstanding IO. need_suspend=1
    corresponds to the previous call to dm_suspend_and_flush().
    
    With this change, the mpath device is only suspended after
    removing partmaps, avoiding a possible deadlock. The sequence
    of actions is 1) clear queue_if_no_path; 2) remove partitions;
    3) suspend; 4) remove.

--- libmultipath/devmapper.c
+++ libmultipath/devmapper.c
@@ -715,9 +715,12 @@ partmap_in_use(const char *name, void *data)
 }
 
 extern int
-_dm_flush_map (const char * mapname)
+_dm_flush_map (const char * mapname, int need_suspend)
 {
 	int r;
+	int queue_if_no_path = 0;
+	unsigned long long mapsize;
+	char params[PARAMS_SIZE] = {0};
 
 	if (!dm_map_present(mapname))
 		return 0;
@@ -729,6 +732,16 @@ _dm_flush_map (const char * mapname)
 	if (partmap_in_use(mapname, NULL))
 		return 1;
 
+	if (need_suspend &&
+	    !dm_get_map(mapname, &mapsize, params) &&
+	    strstr(params, "queue_if_no_path")) {
+		if (!dm_queue_if_no_path((char *)mapname, 0))
+			queue_if_no_path = 1;
+		else
+			/* Leave queue_if_no_path alone if unset failed */
+			queue_if_no_path = -1;
+	}
+
 	if (dm_remove_partmaps(mapname))
 		return 1;
 
@@ -737,49 +750,22 @@ _dm_flush_map (const char * mapname)
 		return 1;
 	}
 
+	if (need_suspend && queue_if_no_path != -1)
+		dm_simplecmd_flush(DM_DEVICE_SUSPEND, mapname, 0);
+
 	r = dm_simplecmd_flush(DM_DEVICE_REMOVE, mapname, 0);
 
 	if (r) {
 		condlog(4, "multipath map %s removed", mapname);
 		return 0;
+	} else {
+		condlog(2, "failed to remove multipath map %s", mapname);
+		if (need_suspend && queue_if_no_path != -1) {
+			dm_simplecmd_noflush(DM_DEVICE_RESUME, mapname, 0);
+			if (queue_if_no_path == 1)
+				dm_queue_if_no_path((char *)mapname, 1);
+		}
 	}
-	return 1;
-}
-
-extern int
-dm_suspend_and_flush_map (const char * mapname)
-{
-	int s = 0, queue_if_no_path = 0;
-	unsigned long long mapsize;
-	char params[PARAMS_SIZE] = {0};
-
-	if (!dm_map_present(mapname))
-		return 0;
-
-	if (dm_type(mapname, TGT_MPATH) <= 0)
-		return 0; /* nothing to do */
-
-	if (!dm_get_map(mapname, &mapsize, params)) {
-		if (strstr(params, "queue_if_no_path"))
-			queue_if_no_path = 1;
-	}
-
-	if (queue_if_no_path)
-		s = dm_queue_if_no_path((char *)mapname, 0);
-	/* Leave queue_if_no_path alone if unset failed */
-	if (s)
-		queue_if_no_path = 0;
-	else
-		s = dm_simplecmd_flush(DM_DEVICE_SUSPEND, mapname, 0);
-
-	if (!dm_flush_map(mapname)) {
-		condlog(4, "multipath map %s removed", mapname);
-		return 0;
-	}
-	condlog(2, "failed to remove multipath map %s", mapname);
-	dm_simplecmd_noflush(DM_DEVICE_RESUME, mapname, 0);
-	if (queue_if_no_path)
-		s = dm_queue_if_no_path((char *)mapname, 1);
 	return 1;
 }
 
