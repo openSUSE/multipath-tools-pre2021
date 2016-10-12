2a14c6a63c7c67641c97b1b097424732508b3724
Martin Wilck <mwilck@suse.de>
2016-10-12 15:39:46 +0200

    multipathd: use weaker "force_reload" at startup
    "force_reload" has originally been created to support multipath -r
    (admin triggered reload). multipathd also uses this parameter
    in DAEMON_CONFIGURE state, too. But this is causes unnecessary
    device-mapper ioctls when multipathd is first started, if existing
    maps have been discovered (perhaps set up by previous instance of
    multipathd, e.g. in the initrd).
    
    This patch introduces a weaker form of the force_reload parameter
    to coalesce_paths(). The parameter can now take three values:
     * FORCE_RELOAD_NONE: existing maps aren't touched at all
     * FORCE_RELOAD_YES: all maps are rebuilt from scratch and (re)loaded in DM
     * FORCE_RELOAD_WEAK: existing maps are compared to the current conf and only
       reloaded in DM if there's a difference. This is useful during startup.
    
    References: bsc#998906, bsc#998893, bsc#1005763, bsc#1011400

--- libmultipath/configure.c
+++ libmultipath/configure.c
@@ -747,6 +747,13 @@ out:
 	return ret;
 }
 
+/*
+ * The force_reload parameter determines how coalesce_paths treats existing maps.
+ * FORCE_RELOAD_NONE: existing maps aren't touched at all
+ * FORCE_RELOAD_YES: all maps are rebuilt from scratch and (re)loaded in DM
+ * FORCE_RELOAD_WEAK: existing maps are compared to the current conf and only
+ * reloaded in DM if there's a difference. This is useful during startup.
+ */
 extern int
 coalesce_paths (struct vectors * vecs, vector newmp, char * refwwid, int force_reload, enum mpath_cmds cmd)
 {
@@ -766,7 +773,7 @@ coalesce_paths (struct vectors * vecs, vector newmp, char * refwwid, int force_r
 	if (refwwid && !strlen(refwwid))
 		refwwid = NULL;
 
-	if (force_reload) {
+	if (force_reload != FORCE_RELOAD_NONE) {
 		vector_foreach_slot (pathvec, pp1, k) {
 			pp1->mpp = NULL;
 		}
@@ -853,7 +860,8 @@ coalesce_paths (struct vectors * vecs, vector newmp, char * refwwid, int force_r
 		if (cmd == CMD_DRY_RUN)
 			mpp->action = ACT_DRY_RUN;
 		if (mpp->action == ACT_UNDEF)
-			select_action(mpp, curmp, force_reload);
+			select_action(mpp, curmp,
+				      force_reload == FORCE_RELOAD_YES ? 1 : 0);
 
 		r = domap(mpp, params, is_daemon);
 
