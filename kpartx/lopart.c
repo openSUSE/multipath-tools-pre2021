896e4e2dd2661e63e539cf9d035c0f29fcdc0582
Martin Wilck <mwilck@suse.com>
2017-05-04 17:54:58 +0200

    kpartx: remove is_loop_device
    This function is not used any more.

--- kpartx/lopart.c
+++ kpartx/lopart.c
@@ -63,38 +63,6 @@ xstrdup (const char *s)
 	return t;
 }
 
-extern int
-is_loop_device (const char *device)
-{
-	struct stat statbuf;
-	int loopmajor;
-#if 1
-	loopmajor = 7;
-#else
-	FILE *procdev;
-	char line[100], *cp;
-
-	loopmajor = 0;
-
-	if ((procdev = fopen(PROC_DEVICES, "r")) != NULL) {
-
-		while (fgets (line, sizeof(line), procdev)) {
-
-			if ((cp = strstr (line, " loop\n")) != NULL) {
-				*cp='\0';
-				loopmajor=atoi(line);
-				break;
-			}
-		}
-
-		fclose(procdev);
-	}
-#endif
-	return (loopmajor && stat(device, &statbuf) == 0 &&
-		S_ISBLK(statbuf.st_mode) &&
-		major(statbuf.st_rdev) == loopmajor);
-}
-
 #define SIZE(a) (sizeof(a)/sizeof(a[0]))
 
 extern char *
