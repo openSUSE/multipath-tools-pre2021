opensvc/multipath-tools

commit 2be1fbf06c90837415a86502fc6763f9fe793da5

libmultipath: sanitize fd handling
One should remember that '0' _is_ a valid fd, so we need to set
the fd to '-1' upon allocating the path structure and ensure we're
checking for a value _smaller_ than 0 to detect an invalid fd.

Signed-off-by: Hannes Reinecke <hare@suse.com>

libmultipath/checkers.c
@@ -146,7 +146,7 @@ struct checker * add_checker (char *multipath_dir, char * name)
 	if (!c->repair)
 		goto out;
 
-	c->fd = 0;
+	c->fd = -1;
 	c->sync = 1;
 	list_add(&c->node, &checkers);
 	return c;
@@ -237,7 +237,7 @@ int checker_check (struct checker * c)
 		MSG(c, "checker disabled");
 		return PATH_UNCHECKED;
 	}
-	if (c->fd <= 0) {
+	if (c->fd < 0) {
 		MSG(c, "no usable fd");
 		return PATH_WILD;
 	}

libmultipath/print.c
@@ -1677,7 +1677,7 @@ int snprint_status(char *buff, int len, struct vectors *vecs)
 	int monitored_count = 0;
 
 	vector_foreach_slot(vecs->pathvec, pp, i)
-		if (pp->fd != -1)
+		if (pp->fd >= 0)
 			monitored_count++;
 	fwd += snprintf(buff + fwd, len - fwd, "\npaths: %d\nbusy: %s\n",
 			monitored_count, is_uevent_busy()? "True" : "False");

openSUSE/multipath-tools-pre2021

commit a32ddd6a756b55eff993c982fe43c7d8493ea7a0

libmultipath: ensure checker->fd == -1 if not set
Since 2be1fbf06c90 "libmultipath: sanitize fd handling", we treat
fd == 0 as a valid value. Make sure that this value isn't accidentally
set. Also, fix one more case where fd == 0 was considered invalid.

Fixes: 2be1fbf06c90 libmultipath: sanitize fd handling

libmultipath/checkers.c
@@ -44,6 +44,7 @@ struct checker * alloc_checker (void)
 	if (c) {
 		INIT_LIST_HEAD(&c->node);
 		c->refcount = 1;
+		c->fd = -1;
 	}
 	return c;
 }
@@ -203,6 +204,12 @@ int checker_init (struct checker * c, void ** mpctxt_addr)
 	return 0;
 }
 
+void checker_clear (struct checker *c)
+{
+	memset(c, 0x0, sizeof(struct checker));
+	c->fd = -1;
+}
+
 void checker_put (struct checker * dst)
 {
 	struct checker * src;
@@ -212,7 +219,7 @@ void checker_put (struct checker * dst)
 	src = checker_lookup(dst->name);
 	if (dst->free)
 		dst->free(dst);
-	memset(dst, 0x0, sizeof(struct checker));
+	checker_clear(dst);
 	free_checker(src);
 }
 

libmultipath/checkers.h
@@ -11,7 +11,7 @@
  *
  * PATH_WILD:
  * - Use: None of the checkers (returned if we don't have an fd)
- * - Description: Corner case where "fd <= 0" for path fd (see checker_check())
+ * - Description: Corner case where "fd < 0" for path fd (see checker_check())
  *
  * PATH_UNCHECKED:
  * - Use: Only in directio checker
@@ -128,6 +128,7 @@ void cleanup_checkers (void);
 struct checker * add_checker (char *, char *);
 struct checker * checker_lookup (char *);
 int checker_init (struct checker *, void **);
+void checker_clear (struct checker *);
 void checker_put (struct checker *);
 void checker_reset (struct checker *);
 void checker_set_sync (struct checker *);

libmultipath/checkers/cciss_tur.c
@@ -73,7 +73,7 @@ int libcheck_check(struct checker * c)
 	LogvolInfo_struct    lvi;       // logical "volume" info
 	IOCTL_Command_struct cic;       // cciss ioctl command
 
-	if ((c->fd) <= 0) {
+	if ((c->fd) < 0) {
 		MSG(c,"no usable fd");
 		ret = -1;
 		goto out;

libmultipath/discovery.c
@@ -1573,7 +1573,7 @@ get_state (struct path * pp, struct config *conf, int daemon, int oldstate)
 		}
 		checker_set_fd(c, pp->fd);
 		if (checker_init(c, pp->mpp?&pp->mpp->mpcontext:NULL)) {
-			memset(c, 0x0, sizeof(struct checker));
+			checker_clear(c);
 			condlog(3, "%s: checker init failed", pp->dev);
 			return PATH_UNCHECKED;
 		}

libmultipath/structs.c
@@ -99,6 +99,7 @@ alloc_path (void)
 		pp->fd = -1;
 		pp->tpgs = TPGS_UNDEF;
 		pp->priority = PRIO_UNDEF;
+		checker_clear(&pp->checker);
 	}
 	return pp;
 }