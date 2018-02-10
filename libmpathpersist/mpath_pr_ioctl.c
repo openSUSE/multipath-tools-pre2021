5884932589bab4ee1f4d49fcb809b583af2e343d
Hannes Reinecke <hare@suse.com>
2018-02-10 12:19:03 +0100

    libmpathpersist/mpath_pr_ioctl: sanitize transport ID decoding
    The transport ID cannot contain several descriptors, so we don't
    need to loop in decode_transport_id(). And we should be validating
    the length of the transport ID agains the overall length of the
    buffer to avoid a buffer overflow.
    
    Found by coverity.
    
    Signed-off-by: Hannes Reinecke <hare@suse.com>

--- libmpathpersist/mpath_pr_ioctl.c
+++ libmpathpersist/mpath_pr_ioctl.c
@@ -247,7 +247,8 @@ void mpath_format_readfullstatus(struct prin_resp *pr_buff, int len, int noisy)
 
 		tid_len_len = ((p[20] << 24) | (p[21] << 16) |
 				(p[22] << 8) | p[23]);
-
+		if (tid_len_len > additional_length - k)
+			tid_len_len = additional_length - k;
 		if (tid_len_len > 0)
 			decode_transport_id( &fdesc, &p[24], tid_len_len);
 
@@ -266,29 +267,25 @@ void mpath_format_readfullstatus(struct prin_resp *pr_buff, int len, int noisy)
 void
 decode_transport_id(struct prin_fulldescr *fdesc, unsigned char * p, int length)
 {
-	int num, k;
-	int jump;
-	for (k = 0, jump = 24; k < length; k += jump, p += jump) {
-		fdesc->trnptid.format_code = ((p[0] >> 6) & 0x3);
-		fdesc->trnptid.protocol_id = (p[0] & 0xf);
-		switch (fdesc->trnptid.protocol_id) {
-		case MPATH_PROTOCOL_ID_FC:
-			memcpy(&fdesc->trnptid.n_port_name, &p[8], 8);
-			jump = 24;
-			break;
-		case MPATH_PROTOCOL_ID_ISCSI:
-			num = ((p[2] << 8) | p[3]);
-			memcpy(&fdesc->trnptid.iscsi_name, &p[4], num);
-			jump = (((num + 4) < 24) ? 24 : num + 4);
-			break;
-		case MPATH_PROTOCOL_ID_SAS:
-			memcpy(&fdesc->trnptid.sas_address, &p[4], 8);
-			jump = 24;
-			break;
-		default:
-			jump = 24;
-			break;
-		}
+	int num;
+
+	fdesc->trnptid.format_code = ((p[0] >> 6) & 0x3);
+	fdesc->trnptid.protocol_id = (p[0] & 0xf);
+	switch (fdesc->trnptid.protocol_id) {
+	case MPATH_PROTOCOL_ID_FC:
+		memcpy(&fdesc->trnptid.n_port_name, &p[8], 8);
+		break;
+	case MPATH_PROTOCOL_ID_ISCSI:
+		num = ((p[2] << 8) | p[3]);
+		if (num + 4 > length)
+			num = length - 4;
+		memcpy(&fdesc->trnptid.iscsi_name, &p[4], num);
+		break;
+	case MPATH_PROTOCOL_ID_SAS:
+		memcpy(&fdesc->trnptid.sas_address, &p[4], 8);
+		break;
+	default:
+		break;
 	}
 }
 
