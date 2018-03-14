22221a02fd237a818bce2e3fd9169ba4dba9a646
Benjamin Marzinski <bmarzins@redhat.com>
2018-03-14 12:46:38 -0500

    libmultipath: fix log_pthread processing
    log_pthread() was waiting for notification on logev_cond, without
    checking if it had already happened.  This means it could end up
    waiting, while there is work that it should be doing.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>
    (cherry picked from commit f2c8d994aa7ba8e0e10a609851d6e4cc9ef99fcd)

--- libmultipath/log_pthread.h
+++ libmultipath/log_pthread.h
@@ -9,8 +9,6 @@ extern pthread_mutex_t logq_lock;
 extern pthread_mutex_t logev_lock;
 extern pthread_cond_t logev_cond;
 
-extern int logq_running;
-
 void log_safe(int prio, const char * fmt, va_list ap);
 void log_thread_start(pthread_attr_t *attr);
 void log_thread_stop(void);
