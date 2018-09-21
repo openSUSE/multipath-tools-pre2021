fee2b8257515bfa626aa6b9f22ef65ec1efe4db9
Benjamin Marzinski <bmarzins@redhat.com>
2018-09-21 18:05:23 -0500

    multipathd: minor fixes
    Also, in ev_remove_path(), strncpy() could end up unterminated, so
    use strlcpy() instead.
    
    Signed-off-by: Benjamin Marzinski <bmarzins@redhat.com>

--- multipathd/main.c
+++ multipathd/main.c
@@ -915,7 +915,7 @@ ev_remove_path (struct path *pp, struct vectors * vecs, int need_do_map)
 			/*
 			 * flush_map will fail if the device is open
 			 */
-			strncpy(alias, mpp->alias, WWID_SIZE);
+			strlcpy(alias, mpp->alias, WWID_SIZE);
 			if (mpp->flush_on_last_del == FLUSH_ENABLED) {
 				condlog(2, "%s Last path deleted, disabling queueing", mpp->alias);
 				mpp->retry_tick = 0;
