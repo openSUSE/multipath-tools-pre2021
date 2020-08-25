7a58a3e862d16e6103007c0c114d7270cc8417a1
Martin Wilck <mwilck@suse.com>
2020-08-25 23:12:32 +0200

    multipath-tools tests: fix memory leak in hwtable test

--- tests/hwtable.c
+++ tests/hwtable.c
@@ -25,6 +25,7 @@
 #include "test-lib.h"
 #include "print.h"
 #include "util.h"
+#include "foreign.h"
 
 #define N_CONF_FILES 2
 
@@ -187,6 +188,9 @@ static int teardown(void **state)
 
 	free_hwt(*state);
 	*state = NULL;
+	cleanup_prio();
+	cleanup_checkers();
+	cleanup_foreign();
 
 	return 0;
 }
@@ -468,6 +472,7 @@ static void replicate_config(const struct hwt_state *hwt, bool local)
 		/* "local" configuration */
 		hwtable = get_used_hwes(hwt->vecs->pathvec);
 		cfg1 = snprint_config(conf, NULL, hwtable, hwt->vecs->mpvec);
+		vector_free(hwtable);
 	}
 
 	assert_non_null(cfg1);
